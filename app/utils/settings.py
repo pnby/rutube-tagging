0287a13bf5641b668a29a72c9c163ec6f3ae3e58
pnby <ryda1111111@gmail.com>
2024-09-29 03:43:26 +0500

    awesome commit

--- app/utils/settings.py
+++ app/utils/settings.py
@@ -3,7 +3,7 @@ import os
 from typing_extensions import Literal
 
 # a constant that stores some data
-available_llm_models: Literal["mistral"] = "mistral"
+available_llm_models: Literal["llama3"] = "llama3"
 available_stt_models: Literal["base", "small"] = "small"
 available_stt_languages: Literal["ru"] = "ru"
 ALLOWED_VIDEO_TYPES = ["video/mp4", "video/x-matroska", "video/avi", "video/webm"]
