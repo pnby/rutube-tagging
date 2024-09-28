77395977d1f3fe57d0acc41189eabfe9ce593f48
pnby <ryda1111111@gmail.com>
2024-09-29 03:27:06 +0500

    awesome commit

--- app/__main__.py
+++ app/__main__.py
@@ -1,10 +1,13 @@
-from transformers import GPT2Tokenizer, GPT2LMHeadModel
+import logging
 
-model_name_or_path = "sberbank-ai/rugpt3large_based_on_gpt2"
-tokenizer = GPT2Tokenizer.from_pretrained(model_name_or_path)
-model = GPT2LMHeadModel.from_pretrained(model_name_or_path).cuda()
-text = "Александр Сергеевич Пушкин родился в "
-input_ids = tokenizer.encode(text, return_tensors="pt").cuda()
-out = model.generate(input_ids.cuda())
-generated_text = list(map(tokenizer.decode, out))[0]
-print(generated_text)
+from fastapi import FastAPI
+
+from app.routes.tags import router as tag_router
+
+logging.basicConfig(
+    level=logging.DEBUG,
+    format="%(filename)s:%(lineno)d #%(levelname)-8s [%(asctime)s] - %(name)s - %(message)s",
+)
+
+app = FastAPI()
+app.include_router(tag_router, prefix="/v1/api")
